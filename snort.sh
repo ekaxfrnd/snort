2676bbb9e0e69adac2626386fc0344d50734b2c5
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:17:51 +0800

    add change directory back

--- snort.sh
+++ snort.sh
@@ -3,4 +3,4 @@
 wget https://www.snort.org/downloads/snort/snort-2.9.17.1.tar.gz
 tar -zxvf snort-2.9.17.1
 cd snort-2.9.17.1
-./configure --sourcefire && make && sudo make install
+./configure --sourcefire && make && sudo make install && cd ..
