8b2fdbe5801d098b7e81563603b8037ee7098c32
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:18:35 +0800

    main file

--- install.sh
+++ install.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
+bash prep.sh
 bash daq.sh
 bash snort.sh
-
-sudo ldconfig
-
-sudo ln -s /usr/local/bin/snort /usr/sbin/snort
-
-sudo groupadd snort
-sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
-
-sudo mkdir -p /etc/snort/rules
-sudo mkdir /var/log/snort
-sudo mkdir /usr/local/lib/snort_dynamicrules
-
-sudo chmod -R 5775 /etc/snort
-sudo chmod -R 5775 /var/log/snort
-sudo chmod -R 5775 /usr/local/lib/snort_dynamicrules
-sudo chown -R snort:snort /etc/snort
-sudo chown -R snort:snort /var/log/snort
-sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules
-
-sudo touch /etc/snort/rules/white_list.rules
-sudo touch /etc/snort/rules/black_list.rules
-sudo touch /etc/snort/rules/local.rules && cd ..
+bash config.sh
 
 
 
