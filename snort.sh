0ed5aa1d09216da8694578d6e0e0304c407e02e9
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:25:40 +0800

    fix file extension

--- snort.sh
+++ snort.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://www.snort.org/downloads/snort/snort-2.9.17.1.tar.gz
-tar -zxvf snort-2.9.17.1
+tar -zxvf snort-2.9.17.1.gz
 cd snort-2.9.17.1
 ./configure --sourcefire && make && sudo make install && cd ..
