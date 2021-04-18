87b3fc7278dc581d37333b29c0c735db87be6680
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:17:00 +0800

    split config file

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo ldconfig
+sudo ln -s /usr/local/bin/snort /usr/sbin/snort
+sudo groupadd snort
+sudo useradd snort -r -s /sbin/nologin -c SNORT_IDS -g snort
+sudo mkdir -p /etc/snort/rules
+sudo mkdir /var/log/snort
+sudo mkdir /usr/local/lib/snort_dynamicrules
+sudo chmod -R 5775 /etc/snort
+sudo chmod -R 5775 /var/log/snort
+sudo chmod -R 5775 /usr/local/lib/snort_dynamicrules
+sudo chown -R snort:snort /etc/snort
+sudo chown -R snort:snort /var/log/snort
+sudo chown -R snort:snort /usr/local/lib/snort_dynamicrules
+sudo touch /etc/snort/rules/white_list.rules
+sudo touch /etc/snort/rules/black_list.rules
+sudo touch /etc/snort/rules/local.rules
+sudo cp snort-2.9.17.1/etc/*.conf*
+sudo cp snort-2.9.17.1/etc/*.map
