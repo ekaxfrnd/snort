322c41503f581b311c54c8c5a53dbfe852134517
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:34:52 +0800

    i forgot to add --enable-sourcefire :(

--- snort.sh
+++ snort.sh
@@ -3,4 +3,4 @@
 wget https://www.snort.org/downloads/snort/snort-2.9.17.1.tar.gz
 tar -zxvf snort-2.9.17.1.tar.gz
 cd snort-2.9.17.1
-./configure --sourcefire && make && sudo make install && cd ..
+./configure --enable-sourcefire && make && sudo make install && cd ..
