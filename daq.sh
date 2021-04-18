c5cce9b27b37ea8763ec15591547005396ba4f8e
ekagustimann <ekaxfrnd@gmail.com>
2021-04-19 05:17:22 +0800

    fix error disable dependency tracking

--- daq.sh
+++ daq.sh
@@ -4,4 +4,4 @@ wget https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz
 tar -zxvf daq-2.0.7.tar.gz
 cd daq-2.0.7
 autoreconf -f -i
-./configure && make && sudo make install
+./configure && make && sudo make install && cd ..
